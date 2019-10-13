1800b828b1f9cf8d464a55874b8256e984d4b13b
takamuraTK <tirpitz.zzz@outlook.jp>
2019-10-13 07:50:44 +0000

    レビューのいいね機能にAjaxの対応

--- app/controllers/reviewfavorites_controller.rb
+++ app/controllers/reviewfavorites_controller.rb
@@ -1,15 +1,11 @@
 class ReviewfavoritesController < ApplicationController
   def create
-    review = Review.find(params[:review_id])
-    current_user.addreviewfav(review)
-    flash[:success] = "いいねしました！"
-    redirect_back(fallback_location: root_path)
+    @review = Review.find(params[:review_id])
+    current_user.addreviewfav(@review)
   end
 
   def destroy
-    review = Review.find(params[:review_id])
-    current_user.removereviewfav(review)
-    flash[:success] = "いいねを解除しました！"
-    redirect_back(fallback_location: root_path)
+    @review = Review.find(params[:review_id])
+    current_user.removereviewfav(@review)
   end
 end
