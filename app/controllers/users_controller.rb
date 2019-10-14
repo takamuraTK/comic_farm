442f6be7be3415eb9f68add45837a53ada4bd667
takamuraTK <tirpitz.zzz@outlook.jp>
2019-10-14 05:23:53 +0000

    シリーズ名にドットが含まれているときボタンが開閉しないのを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
       @reviews = Review.where(user_id: params[:id])
       @favs = @user.favbooks.page(params[:page]).per(10)
       @user.books
+      @recently_subs_id = @user.subscribes.order("id DESC").limit(4).select("book_id")
       @subs_series = @user.books.group(:series).count.sort
       @favs_series = @user.favbooks.group(:series).count.sort
     end
