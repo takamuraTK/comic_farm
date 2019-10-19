2a78504a4b3bec3c71fcde0276e5c3b8b800f080
takamuraTK <tirpitz.zzz@outlook.jp>
2019-10-19 15:37:56 +0000

    reviewfavoriteのバリデーション追加

--- app/models/reviewfavorite.rb
+++ app/models/reviewfavorite.rb
@@ -1,4 +1,7 @@
 class Reviewfavorite < ApplicationRecord
   belongs_to :user
   belongs_to :review
+  validates :user_id, presence: true
+  validates :review_id, presence: true
+  validates :user_id, :uniqueness => {:scope => :review_id}
 end
