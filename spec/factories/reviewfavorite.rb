2700621991cea0215d2bf08887ad11d796f74bda
takamuraTK <tirpitz.zzz@outlook.jp>
2019-10-19 15:38:21 +0000

    reviewfavoriteのテスト追加

---
+++ spec/factories/reviewfavorite.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :reviewfavorite do
+    user
+    review
+  end
+end
---
+++ spec/models/reviewfavorite_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Reviewfavorite, type: :model do
+  it "有効なReviewfavoriteレコードが登録できること" do
+    expect(FactoryBot.build(:reviewfavorite)).to be_valid
+  end
+
+  it "user_idがないときは無効であること" do
+    expect(FactoryBot.build(:reviewfavorite, user_id: nil)).to be_invalid
+  end
+
+  it "review_idがないときは無効であること" do
+    expect(FactoryBot.build(:reviewfavorite, review_id: nil)).to be_invalid
+  end
+
+  it "user_idとreview_idの組み合わせが重複しているときは無効であること" do
+    user = FactoryBot.create(:user)
+    review = FactoryBot.create(:review)
+    FactoryBot.create(
+      :reviewfavorite,
+      user: user,
+      review: review
+    )
+    other_reviewfavorite = FactoryBot.build(
+      :reviewfavorite,
+      user: user,
+      review: review
+    )
+    other_reviewfavorite.valid?
+    expect(other_reviewfavorite.errors[:user_id]).to include 'はすでに存在します'
+  end
+end
