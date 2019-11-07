8430ed8a6a3d3f67100945c939945c2aa1549b88
takamuraTK <tirpitz.zzz@outlook.jp>
2019-11-07 15:31:59 +0900

    indexページの追加、サイトタイトルのデザイン修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,8 +2,6 @@ class ToppagesController < ApplicationController
   def index
     if user_signed_in?
       redirect_to user_path(current_user.id)
-    else
-      redirect_to user_session_path
     end
   end
   
