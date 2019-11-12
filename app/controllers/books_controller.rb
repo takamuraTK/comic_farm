731f4fe1a08746f029e6a4c2082ba9ebacc17fd1
takamuraTK <tirpitz.zzz@outlook.jp>
2019-11-13 05:17:34 +0900

    ランキングを上位30件までの表示に変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,11 @@ class BooksController < ApplicationController
   before_action :require_sign_in
   def new
     @books = []
-    params[:sortselect].presence || 'standard'
     @page = params[:pageselect].presence || 1
-    if params[:title].present?
-      view_context.search_books(params[:title], params[:sortselect], @page)
-      @no_results = '漫画は見つかりませんでした。' if @books.blank?
-    end
-    @books = Kaminari.paginate_array(@books).page(params[:page]).per(30)
+    return unless params[:title].present?
+
+    view_context.search_books(params[:title], params[:sortselect], @page)
+    @no_results = '漫画は見つかりませんでした。' if @books.blank?
   end
 
   def show
@@ -23,15 +21,13 @@ class BooksController < ApplicationController
 
   def ranking
     book_subs_ids = Hash[Subscribe.group(:book_id).count.sort_by { |_, v| -v }].keys
-    @book_ranking = Book.where(id: book_subs_ids).order("FIELD(id, #{book_subs_ids.join(',')})").page(params[:page]).per(10)
-    params[:page].nil? ? (@rank = 1) : (@rank = (params[:page].to_i - 1) * 10 + 1)
+    @book_ranking = Book.where(id: book_subs_ids).order("FIELD(id, #{book_subs_ids.join(',')})").limit(30)
   end
 
   def review_ranking
     @book_review_average = Book.joins(:reviews).group(:book_id).average(:point)
     book_review_ids = Hash[@book_review_average.sort_by { |_, v| -v }].keys
-    @review_ranking = Book.where(id: book_review_ids).order("FIELD(id, #{book_review_ids.join(',')})").page(params[:page]).per(10)
-    params[:page].nil? ? (@rank = 1) : (@rank = (params[:page].to_i - 1) * 10 + 1)
+    @review_ranking = Book.where(id: book_review_ids).order("FIELD(id, #{book_review_ids.join(',')})").limit(30)
   end
 
   private
