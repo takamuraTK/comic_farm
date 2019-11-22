2cddf536643b6e1fcb9e2b27fdca96f5d842d43a
takamuraTK <tirpitz.zzz@outlook.jp>
2019-11-22 15:34:12 +0900

    連携ログインのコントローラーにあるprivateが使用用途がないものであったので削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -41,23 +41,5 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # redirect_to new_user_registration_url
       # ↑ ログインすることになるので以下のように修正
     end
-end
-
-  private
-
-  def callback_from(provider)
-    provider = provider.to_s
-
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      print('persisted true')
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      sign_in_and_redirect @user, event: :authentication
-    else
-      print('persisted false')
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to controller: 'sessions', action: 'new'
-    end
   end
 end
