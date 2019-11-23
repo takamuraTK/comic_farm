e03bc97566d76bc12834fae7e3dac407fa687078
takamuraTK <tirpitz.zzz@outlook.jp>
2019-11-23 11:29:23 +0900

    newlyモデルのダウンロードメソッドを各役割毎に分担し、DRYに修正

--- app/models/newly.rb
+++ app/models/newly.rb
@@ -4,35 +4,21 @@ class Newly < ApplicationRecord
   validates :publisherName, presence: true
   validates :counter, presence: true
   validates :month, presence: true
-  before_validation :get_month_book
+  before_validation :search_month_book
 
-  def get_month_book
-    month = self.month.in_time_zone.strftime('%Y年%m月')
-    pre_month = self.month.in_time_zone.prev_month.strftime('%Y年%m月')
-    check_page = 'running'
-    count = 0
-    page = 0
+  def search_month_book
+    str_month(month)
+    str_pre_month(month)
+    @check_page = 'running'
+    @count = 0
+    @page = 0
 
-    while check_page == 'running'
-      page += 1
-      search_publisher(publisherName, page)
-      @results.each do |result|
-        book = Book.new(read(result))
-        if book.salesDate.include?(pre_month)
-          check_page = 'stop'
-          break
-        end
-
-        next unless book.salesDate.include?(month)
-
-        comic = Book.find_or_initialize_by(isbn: book.isbn)
-        next if comic.persisted?
-
-        count += 1 if book.save
-      end
+    while @check_page == 'running'
+      @page += 1
+      search_publisher(publisherName, @page)
     end
 
-    self.counter = count
+    self.counter = @count
   end
 
   def search_publisher(publisher, page)
@@ -43,9 +29,42 @@ class Newly < ApplicationRecord
       sort: '-releaseDate',
       page: page
     )
+    check_results(@results)
+  end
+
+  def check_results(results)
+    results.each do |result|
+      @book = Book.new(read(result))
+
+      if @book.salesDate.include?(@pre_month)
+        @check_page = 'stop'
+        break
+      end
+
+      next unless @book.salesDate.include?(@month)
+
+      save_result(@book)
+    end
+    @check_page
+  end
+
+  def save_result(book)
+    comic = Book.find_or_initialize_by(isbn: book.isbn)
+    unless comic.persisted?
+      @count += 1 if book.save
+    end
+    @count
   end
 
   private
+
+  def str_month(month)
+    @month = month.in_time_zone.strftime('%Y年%m月')
+  end
+
+  def str_pre_month(month)
+    @pre_month = month.in_time_zone.prev_month.strftime('%Y年%m月')
+  end
 
   def read(result)
     { title: result['title'],
