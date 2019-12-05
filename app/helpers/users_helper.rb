9c25ea2642b382eac933c3caf5458df979f64c26
takamuraTK <tirpitz.zzz@outlook.jp>
2019-12-05 17:38:20 +0900

    登録漫画をシリーズ出版社毎に色分け

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,27 @@
+module UsersHelper
+  def get_color(comic)
+    publisher = Bookseries.find_by(title: comic).publisher
+    color = case publisher
+            when '集英社'
+              'btn-primary'
+            when '小学館'
+              'btn-outline-dark'
+            when '講談社'
+              'btn-success'
+            when '竹書房'
+              'btn-danger'
+            when '新潮社'
+              'btn-warning'
+            when '双葉社'
+              'btn-light'
+            when '秋田書店'
+              'btn-secondary'
+            when '少年画報社'
+              'btn-info'
+
+            else
+              'btn-dark'
+            end
+    color
+  end
+end
